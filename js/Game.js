2da101e99ed26c91ada29e3b2e33f418644e8a6f
VijayWasTaken <88245937+VijayWasTaken@users.noreply.github.com>
2021-11-08 18:34:03 +0000

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,135 @@
+class Game {
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    animal1 = createSprite(width / 2 - 50, height - 100);
+    animal1.addImage(" animal1",  animal1_img);
+    animal1.scale = 0.07;
+
+    animal2 = createSprite(width / 2 + 100, height - 100);
+    animal2.addImage(" animal2",  animal2_img);
+    animal2.scale = 0.07;
+
+    animals = [ animal1,  animal2];
+
+   
+    //create a group for bush and grass
+    bushes=new Group();
+    grasses=new Group();
+
+    // Adding fuel sprite in the game
+    this.addSprites(bushes, 4, bushImage, 0.02);
+
+    // Adding coin sprite in the game
+    this.addSprites(grasses, 18, grassImage, 0.09);
+  }
+
+  // C38 TA
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      x = random(width / 2 + 150, width / 2 - 150);
+      y = random(-height * 4.5, height - 400);
+
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  play() {
+    this.handleElements();
+
+    Player.getPlayersInfo();
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      //index of the array
+      var index = 0;
+      for (var plr in allPlayers) {
+        //add 1 to the index for every loop
+        index = index + 1;
+
+        //use data form the database to display the cars in x and y direction
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+
+        
+
+        animals[index - 1].position.x = x;
+        animals[index - 1].position.y = y;
+
+  
+        if (index === player.index) {
+          stroke(10);
+          //fill the circle with color
+          fill("red");
+          //add the ellipse to create a focus for the player 
+          ellipse(player.positionX,height-player.positionY,150);
+          this.handleBush(index);
+          this.handleGrass(index);
+          camera.position.x=animals[index-1].position.x;
+          camera.position.y=animals[index-1].position.y;
+         }
+      }
+
+      // handling keyboard events
+      if (keyIsDown(UP_ARROW)) {
+        player.positionY += 10;
+        player.update();
+      }
+
+      drawSprites();
+    }
+  }
+
+  handleBush(index) {
+    // Write a overlap function  for animal and bush 
+    animals[index-1].overlap(bushes,function(collector,collected){
+      player.bushes=185;
+      collected.remove();
+    })
+    
+       //remove the bush
+  }
+
+  handleGrass(index) {
+    
+    // Write a overlap function  for animal and grass
+    animals[index-1].overlap(grasses,function(collector,collected){
+      player.score+=21;
+      player.update();
+      collected.remove();
+    })
+    
+       //remove the grass
+  }
+}
